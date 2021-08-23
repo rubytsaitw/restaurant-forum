0ec89ca3e8b022c80ff92dd5820ac9bf7af766ab
Ruby Tsai <hjtsai0307@gmail.com>
2021-08-23 22:27:37 +0800

    install express-session connect-flash, and check for password consistency and email uniqueness

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,13 +7,28 @@ const userController = {
     return res.render('signup')
   },
   signUp: (req, res) => {
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
-    }).then(user => {
-      return res.redirect('/signin')
-    })
+    // confirm password
+    if (req.body.passwordCheck !== req.body.password) {
+      req.flash('error_messages', 'Inconsistent password!')
+      return res.redirect('/signup')
+    } else {
+      // confirm unique user
+      User.findOne({ where: { email: req.body.email } }).then(user => {
+        if (user) {
+          req.flash('error_messages', 'This email is already registered')
+          return res.redirect('/signup')
+        } else {
+          User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
+          }).then(user => {
+            req.flash('success_messages', 'Registration successful')
+            return res.redirect('/signin')
+          })
+        }
+      })
+    }
   }
 }
 
