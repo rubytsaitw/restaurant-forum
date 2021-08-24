3ca053f79608f82212703f1183240189c2cd5ab8
Ruby Tsai <hjtsai0307@gmail.com>
2021-08-24 13:44:23 +0800

    update admin create page for edit use

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -27,9 +27,14 @@ const adminController = {
         res.redirect('/admin/restaurants')
       })
   },
-    getRestaurant: (req, res) => {
+  getRestaurant: (req, res) => {
     return Restaurant.findByPk(req.params.id, { raw: true }).then(restaurant => {
       return res.render('admin/restaurant', { restaurant: restaurant })
+    })
+  },
+  editRestaurant: (req, res) => {
+    return Restaurant.findByPk(req.params.id, { raw: true }).then(restaurant => {
+      return res.render('admin/create', { restaurant: restaurant })
     })
   }
 }
