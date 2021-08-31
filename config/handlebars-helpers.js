83cf760d3dcaae78005126e362186f64bfbf78af
Ruby Tsai <hjtsai0307@gmail.com>
2021-08-31 12:11:16 +0800

    install moment and convert time format

--- config/handlebars-helpers.js
+++ config/handlebars-helpers.js
@@ -1,8 +1,13 @@
+const moment = require('moment')
+
 module.exports = {
   ifCond: function(a, b, options) {
     if (a === b) {
       return options.fn(this)
     }
     return options.inverse(this)
+  },
+  momentFunc: function(datetime) {
+    return moment(datetime).fromNow()
   }
 }
