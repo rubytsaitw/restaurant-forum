f9115d338ac37f7f77bf424a46138f275f33de62
Ruby Tsai <hjtsai0307@gmail.com>
2021-09-03 14:19:42 +0800

    add random avatar to user seed

--- seeders/20210824082842-users-seed-file.js
+++ seeders/20210824082842-users-seed-file.js
@@ -4,26 +4,29 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Users', [{
       id: 5,
+      name: 'root',
       email: 'root@example.com',
       password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
       isAdmin: true,
-      name: 'root',
+      avatar: `https://loremflickr.com/320/240/paris,girl/?lock=${Math.random() * 100}`,
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
       id: 15,
+      name: 'user1',
       email: 'user1@example.com',
       password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
       isAdmin: false,
-      name: 'user1',
+      avatar: `https://loremflickr.com/320/240/paris,girl/?lock=${Math.random() * 100}`,
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
       id: 25,
+      name: 'user2',
       email: 'user2@example.com',
       password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
       isAdmin: false,
-      name: 'user2',
+      avatar: `https://loremflickr.com/320/240/paris,girl/?lock=${Math.random() * 100}`,
       createdAt: new Date(),
       updatedAt: new Date()
     }], {})
